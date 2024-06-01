4cf9395aa03bd2f9831b7479f158c2ff411e6041
Sagar Tamang <sagar.bdr0000@gmail.com>
2024-06-01 17:03:04 +0530

    initial commit

---
+++ therapist/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TherapistConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'therapist'
---
+++ therapist/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render, HttpResponse
+
+def index(request):
+  return render(request, 'index.html')
