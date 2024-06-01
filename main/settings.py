fff51b819867d1648c62d3297526d908d3b46767
Sagar Tamang <sagar.bdr0000@gmail.com>
2024-06-02 00:42:56 +0530

    production latest

--- main/settings.py
+++ main/settings.py
@@ -66,12 +66,12 @@ ASGI_APPLICATION = "main.asgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
