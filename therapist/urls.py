a7f7315ed6c37b89a714f8d6b43652ef6bd05ff9
Sagar Tamang <sagar.bdr0000@gmail.com>
2024-06-01 17:30:47 +0530

    index page compete

--- therapist/urls.py
+++ therapist/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, app
 
 urlpatterns = [
     path('', index, name='index'),
+    path('app/', app, name='app'),
 ]
--- therapist/views.py
+++ therapist/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
   return render(request, 'index.html')
+
+def app(request):
+  return render(request, 'app.html')
