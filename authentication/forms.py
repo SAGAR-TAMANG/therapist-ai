06b82c3ac62291ced1d1ce9683a86555d23fb02f
Sagar Tamang <sagar.bdr0000@gmail.com>
2024-06-07 09:54:25 +0530

    frontend complete for the login signup

--- authentication/forms.py
+++ authentication/forms.py
@@ -3,43 +3,60 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class LoginForm(forms.Form):
-  username = forms.CharField()
-  password = forms.PasswordInput()
-
-class SignUpForm(UserCreationForm):
   username = forms.CharField(
-    widget= forms.TextInput(
+    widget=forms.TextInput(
       attrs={
-        'placeholder': 'Give your username',
-        'class': 'form-control',
+        'placeholder': 'Username',
+        'class': 'form-control mb-2',
+        'autocomplete': 'current-username',
       }
     )
   )
+  password = forms.CharField(
+    widget=forms.PasswordInput(
+      attrs={
+        'placeholder': 'Password',
+        'class': 'form-control mb-2',
+        'autocomplete': 'current-password',
+      }
+    )
+  )
+
+class SignUpForm(UserCreationForm):
+  def __init__(self, *args, **kwargs):
+    super(SignUpForm, self).__init__(*args, **kwargs)
+    self.fields.pop('username')
+
   email = forms.EmailField(
+    label='Email',
     widget=forms.EmailInput(
       attrs={
         'placeholder': 'Give your email',
-        'class': 'form-control',
+        'class': 'form-control mb-2',
+        'autocomplete': 'email',
       }
     )
   )
   password1 = forms.CharField(
+    label='Password',
     widget=forms.PasswordInput(
       attrs={
-        'placeholder': 'Give your password',
-        'class': 'form-control',
-      }
+        'placeholder': 'Password',
+        'class': 'form-control mb-2',
+        'autocomplete': 'new-password',
+        }
     )
   )
   password2 = forms.CharField(
+    label='Confirm Password',
     widget=forms.PasswordInput(
       attrs={
-        'placeholder': 'Give your password',
-        'class': 'form-control',
+        'placeholder': 'Confirm your password',
+        'class': 'form-control mb-2',
+        'autocomplete': 'new-password',        
       }
     )
   )
-
 
   class Meta:
     model = User
--- authentication/views.py
+++ authentication/views.py
@@ -3,7 +3,8 @@ from .forms import LoginForm, SignUpForm
 
 # Create your views here.
 def login_view(request):
-  return render(request, 'accounts/login.html')
+  form = LoginForm()
+  return render(request, 'accounts/login.html', context={'form': form})
 
 def register_user(request):
   form = SignUpForm()
